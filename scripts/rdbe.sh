484fabd11c46dae5b33bad74f37de834ce0dfa35
Andrii Oriekhov <andriyorehov@gmail.com>
2023-11-21 14:52:20 +0200

    update

--- scripts/rdbe.sh
+++ scripts/rdbe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://unix.stackexchange.com/questions/517773/delete-files-same-name-but-different-file-extension/517776
 # TODO: make generic
-find . -type f -name '*.js' -exec sh -c '      
+find . -type f -name '*.js' -not -path './node_modules/*' -exec sh -c '
     for pathname do
         [ -f "${pathname%.js}.ts" ] && rm "$pathname"
     done' sh {} +
