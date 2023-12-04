ed4c3efe38223c0e4d88d84950f8051517b50c3d
Andrii Oriekhov <andriyorehov@gmail.com>
2023-12-04 15:49:21 +0200

    fix fnm installation

--- mangaro-install.sh
+++ mangaro-install.sh
@@ -40,7 +40,7 @@ docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 # node
-curl -fsSL https://fnm.vercel.app/install | bash
+yay -S fnm-bin
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 
 # python
