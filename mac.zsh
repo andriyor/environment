46a90fa40f68df2e57cd8596e9d8f8cfbbe63d29
Andrii Oriekhov <andriyorehov@gmail.com>
2024-01-08 23:02:47 +0200

    update

--- mac.zsh
+++ mac.zsh
@@ -16,3 +16,11 @@ bindkey "\e\eOC" forward-word
 # fnm
 export PATH="/Users/andrejorehov/Library/Application Support/fnm:$PATH"
 eval "`fnm env`"
+
+# pnpm
+export PNPM_HOME="/Users/aoriekhov/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
