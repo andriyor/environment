053a8f147eb32dfe226366d542c1c0cfe8ba0852
Andrii Oriekhov <andriyorehov@gmail.com>
2025-07-09 16:26:13 +0300

    update

--- raspbery/install.sh
+++ raspbery/install.sh
@@ -1,4 +1,5 @@
 scp ~/git/personal/environment/raspbery/cmdline.txt aoriekhov@192.168.0.162:/boot/firmware/cmdline.txt
+scp ~/git/personal/environment/.config/micro/settings.json aoriekhov@192.168.0.162:/home/aoriekhov/.config/micro/settings.json
 
 apt install micro
 apt install bat
