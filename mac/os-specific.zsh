49546f4a22a6d36c6d8a3f3c736cc5ce0d5a1219
Andrii Oriekhov <andriyorehov@gmail.com>
2025-09-07 15:43:34 +0300

    update zsh settings

--- mac/os-specific.zsh
+++ mac/os-specific.zsh
@@ -3,37 +3,12 @@ alias subl='open -a "Sublime Text"'
 redis-server --daemonize yes
 . "$HOME/.cargo/env"
 
-# bun completions
-[ -s "/Users/andrejorehov/.bun/_bun" ] && source "/Users/andrejorehov/.bun/_bun"
-
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
-
 bindkey "\e\eOD" backward-word
 bindkey "\e\eOC" forward-word
-
-# fnm
-export PATH="/Users/andrejorehov/Library/Application Support/fnm:$PATH"
 
 # nvm
 #export NVM_DIR="$HOME/.nvm"
 #[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 #[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-
-# pnpm
-export PNPM_HOME="/Users/aoriekhov/Library/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
-# pnpm end
-
-# golang
-export GOROOT="/opt/homebrew/bin/go"
-export GOPATH="$HOME/go"
-export PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
-
 
 [[ "$TERM_PROGRAM" == "CodeEditApp_Terminal" ]] && . "/Applications/CodeEdit.app/Contents/Resources/codeedit_shell_integration.zsh"
