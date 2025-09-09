acdd608921e86b2567d80395b9e9bbb926f09f20
Andrii Oriekhov <andriyorehov@gmail.com>
2025-09-09 12:10:57 +0300

    update aliases

--- common/alias.zsh
+++ common/alias.zsh
@@ -2,11 +2,17 @@
 alias g='git'
 alias gl="git pull && (git diff --name-only HEAD@{1} HEAD | grep -qE 'package(-lock)?\.json' && ni || echo 'No dependency changes detected')"
 
+# ------------------------------------------------------------------------------
+# basic
+# ------------------------------------------------------------------------------
+alias cd='z'
 alias cat='bat'
 alias l='eza --all --icons'
 alias ll='eza --long --all --git --icons'
-alias ytdl="yt-dlp -f 'bestvideo[ext=mp4][vcodec!^=av0][vcodec!^=av1]+bestaudio[ext=m4a]/mp4/best' --recode-video mp4"
 
+# ------------------------------------------------------------------------------
+# npm/node
+# ------------------------------------------------------------------------------
 alias npmd='rm -rf ~/.npmrc'
 
 # alias ni='npm i --no-audit'
@@ -36,16 +42,22 @@ alias rmbuild="find . -name 'build' -type d -prune -exec rm -rf '{}' +"
 alias checklock1="jq '.dependencies[].resolved' package-lock.json | grep -v"
 alias checklock2="jq '.packages[].resolved' package-lock.json | grep -v"
 
+# log output to file
+alias lo="node ~/scripts/filter_output.js"
+
+alias fjs="npx prettier --print-width=130 --single-quote=true --write"
+
+# ------------------------------------------------------------------------------
+# helpers
+# ------------------------------------------------------------------------------
+
+alias ytdl="yt-dlp -f 'bestvideo[ext=mp4][vcodec!^=av0][vcodec!^=av1]+bestaudio[ext=m4a]/mp4/best' --recode-video mp4"
+
 # process memory `psmem 20128`
 alias psmem='function _psmem() { ps -o rss= -p $(pgrep -g $1) | awk "{sum += \$1} END {print sum / 1048576 \" GB\"}"; }; _psmem'
 
 # top files by line count `tl md 20`
 alias tl='f() { [ -n "$1" ] && find . -type f -name "*.$1" -print0 | xargs -0 wc -l | sort -nr | head -n "${2:-10}"; }; f'
-
-# log output to file
-alias lo="node ~/scripts/filter_output.js"
-
-alias fjs="npx prettier --print-width=130 --single-quote=true --write"
 
 gsm() {
    git log --since="30 days ago" --pretty=format:"%ad|%H" --date=short --numstat |
