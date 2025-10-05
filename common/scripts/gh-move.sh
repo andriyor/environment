1a7d33f623eaa0a3aed5c4072e49580ad44ce98c
Andrii Oriekhov <andriyorehov@gmail.com>
2025-10-05 21:30:28 +0300

    create script to move all repos over org

---
+++ common/scripts/gh-move.sh
@@ -0,0 +1,9 @@
+OWNER="$1"
+NEW_OWNER="${2:-andriyor}"
+
+REPOS=$(gh repo list "$OWNER" --json owner,name  --jq '.[] | "\(.owner.login)/\(.name)"')
+
+for REPO in $REPOS; do
+  echo "➡️  Transferring $REPO ..."
+  gh api "repos/${REPO}/transfer" -f new_owner="$NEW_OWNER" --silent || echo "⚠️ Failed to transfer $REPO"
+done
